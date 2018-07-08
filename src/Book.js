3c5554e577dc73f861f7c317e3e54762c4cf78c1
Michael Kroth <mkroth65@gmail.com>
2018-07-07 21:57:04 -0500

    Created bookshelf-move select component

--- src/Book.js
+++ src/Book.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types';
+import MoveSelect from './Moveselect'
 import Stars from "./Stars";
 
 class Book extends Component {
@@ -18,17 +19,7 @@ class Book extends Component {
               backgroundImage: 'url(' + this.props.book.imageLinks.thumbnail +')'
             }}
           />
-          <div className="book-shelf-changer">
-            <select>
-              <option value="move" disabled>
-                Move to...
-              </option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+        <MoveSelect />  
         </div>
         <div className="book-title">{this.props.book.title}</div>
         <div className="book-authors">{this.props.book.authors[0]}</div>
